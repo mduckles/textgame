101befd4d922e71604798a121199bf6cc1a8c268
Jonah Duckles <jonah@duckles.org>
2015-01-25 12:44:52 -0600

    refactoring

--- textgame.py
+++ textgame.py
@@ -1,10 +1,24 @@
 from os import system
 from random import randint
+import yaml
 
-def say(message):
-  system('say "%s"' % message)
+config = yaml.load(file('config.yaml'))
+races = config['races']
+weapons = config['weapons'].keys()
+classes = config['classes']
+first_monsters = config['first_monsters']
+locations = config['locations']
 
-races = ['Dragonborn', 'Dwarf', 'Eladrin', 'Elf', 'Half Elf', 'Halfling', 'Human', 'Tiefling' ]
+
+def say(message,output=False):
+    ''' Say a message through the Mac `say` command '''
+    system('say "%s"' % message)
+    if output:
+        print message
+
+def pickrand(inlist):
+    ''' Picks a random item from a list'''
+    return inlist[randint(0,len(inlist)-1)]
 
 def character(listof, mode='race'):
     msg = "Which %s would you like to be" % mode
@@ -17,24 +31,13 @@ def character(listof, mode='race'):
     say("You have selected %s" % selection)
     return selection
 
-classes = ['Cleric', 'Wizard', 'Fighter', 'Ranger', 'Rogue', 'Warloc', 'Warlord', 'Paladin', '']
-
 def setupchar():
     race = character(races)
     charclass = character(classes,mode='class')
     say('You will be a %s %s' %(race,charclass))
     return {'race': race, 'charclass': charclass}
 
-#print setupchar()
 character = {'race': 'Eladrin', 'charclass': 'Wizard'}
-
-locations = ['Dungeon', 'Village', 'Castle', 'Forest', 'Lake', 'Desert', 'Mountain', 'Plains', 'Woodland']
-first_monsters = ['Hipogrith', 'Troll', 'Orc', 'Goblins' ]
-weapons = ['spear', 'dagger', 'javelin', 'club', 'long bow', 'short bow', 'short sword', 'long sword']
-
-def pickrand(inlist):
-    ''' Picks a random item from a list'''
-    return inlist[randint(0,len(inlist)-1)]
 
 def pickweapon():
     say("Which weapon would you like to place in your hand?")
@@ -47,17 +50,29 @@ def scene():
     ''' Set a random scene '''
     scene = dict()
     scene['loc'] = pickrand(locations)
-    say('You are in a %s' % scene['loc'])
+    say('You are in a %s' % scene['loc'], output=True)
     scene['monst'] = pickrand(first_monsters)
-    say('There is a %s there' % scene['monst'])
+    say('There is a %s there' % scene['monst'], output=True)
     scene['weap'] = pickweapon()
-    say("You can attack first.")
+    say("You can attack first.", output=True)
     attack(scene)
 
+def weapon(weap):
+    about = config['weapons'][weap]
+    print "%s can attack at %s with up to %s dammage" % (weap, about['range'], about['die'])
+    res = roll(about['die'])
+    print "Your first roll was %s" % res
+
+def roll(d):
+    return randint(1,d)
+
 def attack(scene):
-    say("You are in a %s with a %s, holding a %s" % (
+    say("You are in a %s with a %s, you are holding a %s" % (
     scene['loc'], scene['monst'], scene['weap']
-    ))
+    ), output = True)
+    weapon(scene['weap'])
 
 
-scene()
+#print setupchar()
+#scene()
+weapon('javelin')
